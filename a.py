ae626b5b104defc79573e641ed55b8eb6c3b7b44
manickam2250 <manicchart@gmail.com>
2022-01-26 16:23:24 +0530

    3

--- a.py
+++ a.py
@@ -1,22 +1,25 @@
-# class mobiles:
-#     def __init__(self,brand,price):
-#         self.brand=brand
-#         self.price=price
+# # class mobiles:
+# #     def __init__(self,brand,price):
+# #         self.brand=brand
+# #         self.price=price
 
-#     def __gt__(self,other):
-#         return self.price>other.price
+# #     def __gt__(self,other):
+# #         return self.price>other.price
 
-# m1=mobiles("vivo",10000)
-# m2=mobiles("redmi",12000)
-# print(m1>m2)
+# # m1=mobiles("vivo",10000)
+# # m2=mobiles("redmi",12000)
+# # print(m1>m2)
 
-class book:
-    def __init__(self,pages):
-        self.pages=pages
+# class book:
+#     def __init__(self,pages):
+#         self.pages=pages
 
-    def __add__(self,another):
-        return self.pages + another.pages
+#     def __add__(self,another):
+#         return self.pages + another.pages
 
-thirukural=book(133)
-thiruvasagam=book(200)
-print(thirukural+thiruvasagam)
+# thirukural=book(133)
+# thiruvasagam=book(200)
+# print(thirukural+thiruvasagam)
+
+for i in "manic":
+    print(i)
