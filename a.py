798b867803636d93566e966fe981fa447c205e8f
manickam2250 <manicchart@gmail.com>
2022-01-28 19:40:06 +0530

    today

--- a.py
+++ a.py
@@ -1,2 +1,27 @@
-for i in "manickam":
-    print(i)
+
+class mobiles:
+    def __init__(self,brand,price,offer):
+        self.brand=brand
+        self.price=price
+        self.offer=offer
+
+    def __lt__(self,other):
+        return self.price<other.price
+
+    def __add__(self,other):
+        return self.offer + other.ccoffer
+
+class creditcard:
+    def __init__(self,brand,ccoffer):
+        self.brand=brand
+        self.ccoffer=ccoffer
+        
+
+
+
+
+m1=mobiles("vivo",10000,1000)
+m2=mobiles("samsung",12000,1000)
+cc=creditcard("vivo",500)
+
+print(m1+cc)
