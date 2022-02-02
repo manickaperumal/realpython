94ef9768dd07244e1dcf731ddf29e118bc2056b3
manickam2250 <manicchart@gmail.com>
2022-02-02 12:37:02 +0530

    csv

--- a.py
+++ a.py
@@ -1,10 +1,33 @@
 import csv
-with open("student.csv","w",newline=" ")as file:
-    reynolds=csv.writer(file)
-    reynolds.writerow(["sid","sname","stdaddress"])
-    count=int(input("enter no of studnets"))
-    for i in range(count):
-        sid=int(input("enter student id"))
-        sname=input("enter student name")
-        saddress=input("enter student address")
-        reynolds.writerow(sid,sname,saddress)
+with open("/home/manikam/first.csv","w")as file:
+    v=csv.writer(file)
+   
+    v.writerow(['name',"mblno","email"])
+    members=int(input("given a no for your purpose:"))
+    for i in range(members):
+        name=input("enter your name:")
+        mblno=int(input("enter your mobile no:"))
+        email_id=input("enter your email id:")
+        v.writerow([name,mblno,email_id])
+        print(type(v))
+
+
+import csv
+with open ("/home/manikam/111.txt","r")as file1:
+  with open("/home/manikam/supermarket.csv","w",newline="\n")as file2:
+
+      store=file1.read()
+      store=store.split()
+      pen=csv.writer(file2)
+      pen.writerow(["no","goods","price"])
+      pen.writerow(store)
+
+
+
+
+
+
+
+
+
+
