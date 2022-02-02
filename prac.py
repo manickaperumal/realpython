f88327d445cdf8f764270c4a8fadd007107e0efb
manickam2250 <manicchart@gmail.com>
2022-02-02 16:30:17 +0530

    class value

--- prac.py
+++ prac.py
@@ -1,17 +1,39 @@
-class test:
-    def __init__(self,name,age):
-        self.name=name
-        self.age=age
+#we can acess the class level variable through class name.we can access also through obj name.we can modify .
+
+class supermarket:
     
-    def fun(self):
+    discount=20
+
+    def __init__(self,brand,price):
+        self.brand=brand
+        self.price=price
+        self.discount=supermarket.discount
+
+    def objdetails(self):
         print(self.__dict__)
 
-    def __str__(self):
-      return self
+rice=supermarket("abc",300)
+rice.objdetails()
 
-a=test("manic",21)
-b=test("aswin",21)
-print(a)
+shoe=supermarket("nike",500)
+shoe.discount=30
+
+shoe.objdetails()
+
+#learn--> if we want to change the class value for particular obj ,we can change through the obj name 
+#output:
+
+manikam@manikam:~$ /bin/python3 /home/manikam/pyy/prac.py
+{'brand': 'abc', 'price': 300, 'discount': 20}
+{'brand': 'nike', 'price': 500, 'discount': 30}
+manikam@manikam:~$ 
+
+
+
+
+
+
+
 
 
 
