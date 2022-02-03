6127da924289e004ab206e89e1f9bcae1f2975ce
manickam2250 <manicchart@gmail.com>
2022-02-03 14:14:52 +0530

    task

---
+++ 3-2-22.py
@@ -0,0 +1,64 @@
+import json
+class student:
+    def __init__(self,name,age,medium):
+        self.name=name
+        self.age=age
+        self.medium=medium
+
+ 
+
+    def a(self):
+       store=self.__dict__
+       json1=json.dumps(store)
+       
+       print(type(json1))
+       return json1
+
+    def b (self):
+        print(self.age)
+
+    def c(self):
+          store=stud1.__dict__
+          
+          json1=json.dumps(store,sort_keys=True)
+          return json1
+    
+
+
+stud1=student("manic",21,"tamil")
+print(stud1.a())
+stud1.b()
+json1=stud1.c()
+print(json1)
+print(json1)
+
+
+
+output:
+
+anikam@manikam:~$ cd pyy
+manikam@manikam:~/pyy$ python3 a.py
+<class 'str'>
+{"name": "manic", "age": 21, "medium": "tamil"}
+21
+{"age": 21, "medium": "tamil", "name": "manic"}
+{"age": 21, "medium": "tamil", "name": "manic"}
+
+
+
+
+        
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
