aa981da8a66eaa4670820f29a1414868e2ebe06d
manickam2250 <manicchart@gmail.com>
2022-02-03 15:59:49 +0530

    readcsv

--- 3-2-22.py
+++ 3-2-22.py
@@ -1,26 +1,19 @@
+
+
+
 import csv
-with open("/home/manikam/111.txt","r")as textfile:
-    with open("/home/manikam/bigproject.csv","w",newline="")as csfile:
-        writer=csv.writer(csfile)
-        writer.writerow(["sno","goods","price"])
-
-        no=int(input("how many lines of content do you want:"))
-        for i in range(no):
-            content=textfile.readline()
-            
-
-            content=content.split()
-            writer.writerow(content)
-
+with open("/home/manikam/bigproject.csv","r")as csfile:
+    r=csv.reader(csfile)
+    output=list(r)
+    for line in output:
+     print(line)
 
 output:
+
 manikam@manikam:~$ /bin/python3 /home/manikam/pyy/3-2-22.py
-how many lines of content do you want:3
-
-
-sno	goods	price
-1	sugar	600
-2	curd	500
-3	comb	5
-
+['sno', 'goods', 'price']
+['1', 'sugar', '600']
+['2', 'curd', '500']
+['3', 'comb', '5']
+manikam@manikam:~$ 
 
