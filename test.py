f5d55a4919fe88c49eecbcb407eb0efead336e1b
manickam2250 <manicchart@gmail.com>
2022-02-04 11:32:27 +0530

    name order success

--- test.py
+++ test.py
@@ -6,15 +6,26 @@ class student:
         self.age=age
         self.degree=degree
         self.district=district
-        with open("/home/manikam/888.json","w")as js:
+        with open("/home/manikam/data.json","w")as js:
            global l
            dic=self.__dict__
            l.append(dic)
            json.dump(l,js)
-
-
-
-            
+    @staticmethod
+    def dict():
+       with open("/home/manikam/data.json","r")as js:
+            list=json.load(js)
+            nameslist=[]
+            for i in list:
+                # print (i)
+                dict=i
+                nameslist.append(dict["name"])
+                nameslist.sort()
+            for j in nameslist:
+                print(j)
+                
 stud1=student("manic",21,"Bsc","tuty")
 stud2=student("perumal",22,"Bcom","trichy")
 stud3=student("hari",24,"Msc","kovilpatti")
+stud1.dict()
+
